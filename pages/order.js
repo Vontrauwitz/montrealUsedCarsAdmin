c646b0d16f4384a4b414e27b1c4bbea8cedd783d
Hans Trauwitz <hans.trauwitz@gmail.com>
2024-07-14 14:19:55 -0400

    adding import to order

--- pages/order.js
+++ pages/order.js
@@ -1,5 +1,6 @@
 import { useSession } from 'next-auth/react';
 import Layout from '@/components/Layout';
+import Loader from '@/components/Loader'
 
 export default function Order() {
   const { data: session, status } = useSession();
