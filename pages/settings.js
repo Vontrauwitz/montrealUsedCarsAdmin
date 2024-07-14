08c5a1db2bb682c52ffc5d04dde0951892d2c7d4
Hans Trauwitz <hans.trauwitz@gmail.com>
2024-07-14 14:22:41 -0400

    adding import to settings

--- pages/settings.js
+++ pages/settings.js
@@ -1,5 +1,6 @@
 import { useSession } from 'next-auth/react';
 import Layout from '@/components/Layout';
+import Loader from '@/components/Loader'
 
 export default function Settings() {
   const { data: session, status } = useSession();
